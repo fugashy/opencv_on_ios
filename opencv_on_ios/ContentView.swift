a3592190571b0934f239f08abe1ebc8ee10b8ff2
fugashy <fugashy@icloud.com>
2020-07-12 22:01:24 +0900

    Initial commit

---
+++ opencv_on_ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  opencv_on_ios
+//
+//  Created by fukukazu kawata on 2020/07/12.
+//  Copyright © 2020 fukukazu kawata. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
