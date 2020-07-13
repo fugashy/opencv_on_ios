dc6f52a10a77293556bd8e2417d1e2b1191769a4
fugashy <fugashy@icloud.com>
2020-07-13 12:07:21 +0900

    Add a skeleton class to wrap OpenCV
    OpenCVWrapper.h, .m and the bridge header file.
    
    xcsshareddata/IDEWorkspaceCheck.plist add shared data to workspace and
    save required checking data.

---
+++ opencv_on_ios/OpenCVWrapper.h
@@ -0,0 +1,17 @@
+//
+//  OpenCVWrapper.h
+//  opencv_on_ios
+//
+//  Created by fukukazu kawata on 2020/07/13.
+//  Copyright © 2020 fukukazu kawata. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OpenCVWrapper : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ opencv_on_ios/OpenCVWrapper.m
@@ -0,0 +1,13 @@
+//
+//  OpenCVWrapper.m
+//  opencv_on_ios
+//
+//  Created by fukukazu kawata on 2020/07/13.
+//  Copyright © 2020 fukukazu kawata. All rights reserved.
+//
+
+#import "OpenCVWrapper.h"
+
+@implementation OpenCVWrapper
+
+@end
