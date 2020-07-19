150568a5319903e9c93c73caef22048305a7bdc6
fugashy <fugashy@icloud.com>
2020-07-19 13:10:18 +0900

    Add files to use camera on Swift UI
    These files are copied from
    https://qiita.com/From_F/items/759544896fe89e828898
    
    Thank to From_F

--- opencv_on_ios/ContentView.swift
+++ opencv_on_ios/ContentView.swift
@@ -2,20 +2,70 @@
 //  ContentView.swift
 //  opencv_on_ios
 //
-//  Created by fukukazu kawata on 2020/07/12.
-//  Copyright © 2020 fukukazu kawata. All rights reserved.
+//  https://qiita.com/From_F
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject private var avFoundationVM = CameraViewModel()
+
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            if avFoundationVM.image == nil {
+                Spacer()
+
+                ZStack(alignment: .bottom) {
+                    CALayerView(caLayer: avFoundationVM.previewLayer)
+
+                    Button(action: {
+                        self.avFoundationVM.takePhoto()
+                    }) {
+                        Image(systemName: "camera.circle.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .frame(width: 80, height: 80, alignment: .center)
+                    }
+                    .padding(.bottom, 100.0)
+                }.onAppear {
+                    self.avFoundationVM.startSession()
+                }.onDisappear {
+                    self.avFoundationVM.endSession()
+                }
+
+                Spacer()
+            } else {
+                ZStack(alignment: .topLeading) {
+                    VStack {
+                        Spacer()
+
+                        Image(uiImage: avFoundationVM.image!)
+                        .resizable()
+                        .scaledToFill()
+                        .aspectRatio(contentMode: .fit)
+
+                        Spacer()
+                    }
+                    Button(action: {
+                        self.avFoundationVM.image = nil
+                    }) {
+                            Image(systemName: "xmark.circle.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width: 30, height: 30, alignment: .center)
+                            .foregroundColor(.white)
+                            .background(Color.gray)
+                    }
+                    .frame(width: 80, height: 80, alignment: .center)
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
